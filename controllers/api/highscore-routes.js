5e88dfde5f90830c686f7fe9ffaf50368e9d1c66
mgetz34 <mgetz340@gmail.com>
2022-12-12 14:20:53 -0700

    continued work on the highscore-routes

--- controllers/api/highscore-routes.js
+++ controllers/api/highscore-routes.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 const { Highscore, User } = require("../../models")
-const withAuth = require('../utils/auth');
+const withAuth = require('../../utils/auth');
 
 //Get all high-scores 
-//http://localhost:3001/highscores
+//http://localhost:3001/api/highscores
+//tests good
 router.get("/", async (req, res) => {
     try {
         const highScoreData = await Highscore.findAll({
             include: [{
-                model: User
+                model: User,
+                attributes: ["username"],
             }],
         });
 
@@ -16,10 +18,11 @@ router.get("/", async (req, res) => {
             highScore.get({ plain: true })
         );
 
-        res.render('highscores', {
-            highScores,
-            loggedIn: req.session.loggedIn,
-        });
+        res.json(highScores);
+
+        // res.render('highscores', {
+        //     highScores
+        // });
 
     } catch (err) {
         console.log(err);
@@ -28,16 +31,17 @@ router.get("/", async (req, res) => {
 });
 
 //Get high-score per user
-//http://localhost:3001/highscores/:id
-router.get("/highscores/:id", withAuth, async (req, res) => {
+//http://localhost:3001/api/highscores/username
+router.get("/highscores/username", async (req, res) => {
     try {
-        const highScoreData = await Highscore.findByPk(req.params.id);
+        const highScoreData = await Highscore.findBy(req.params.username);
         if (!highScoreData) {
             res.status(404).json({ message: "No user with this id!" });
             return;
         }
         const highScores = highScoreData.get({ plain: true });
-        res.render('highscores', highScores);
+        // res.render('highscores', highScores);
+        res.json(highScores);
     } catch (err) {
         res.status(500).json(err);
     };
@@ -45,13 +49,13 @@ router.get("/highscores/:id", withAuth, async (req, res) => {
 
 
 //Post high-score 
-//http://localhost:3001/highscores
+//http://localhost:3001/api/highscores
+//tests good
 router.post("/", async (req, res) => {
     try {
         const newScore = await Highscore.create({
             username: req.body.username,
             score: req.body.score,
-            user_id: req.body.user_id,
         });
         res.status(200).json(newScore);
     } catch (err) {
