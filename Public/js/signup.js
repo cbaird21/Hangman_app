7f10817023f30a7ff7395b7e2cc59f621d0446d7
Garrett Anderson <gman.anderson2001@gmail.com>
2022-12-12 12:38:14 -0700

    Added unique key to username in user model

--- Public/js/signup.js
+++ Public/js/signup.js
@@ -1,24 +1,25 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.querySelector('.input_user').value.trim();
-    const password = document.querySelector('.input_pass').value.trim();
+  const username = document.querySelector(".input_user").value.trim();
+  const password = document.querySelector(".input_pass").value.trim();
 
-    if (username && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+  if (username && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert('Failed to sign up.');
-        }
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      console.log(response);
+      alert("Failed to sign up.");
     }
+  }
 };
 
 document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
