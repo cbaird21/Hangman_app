81aabd99cc6e584a75ed21a4c7f9cf162d27f5c6
mgetz34 <mgetz340@gmail.com>
2022-12-12 10:40:32 -0700

    added the contents to index.js (changed from seed.js) to export seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,15 @@
 const sequelize = require("../config/connection");
+const seedWords = require('./wordData');
+const seedHints = require('./hintData');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+
+    await seedWords();
+
+    await seedHints();
+
+    process.exit(0);
+};
+
+seedAll();
