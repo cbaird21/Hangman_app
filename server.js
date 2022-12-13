e279238a2a14a4da76ac8815ab8e859fdb244303
Jenn Marshall <jennyfever@gmail.com>
2022-12-12 19:22:34 -0700

    added app use to server

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "Public")));
+app.use(express.static("assets"));
 
 app.use(routes);
 
