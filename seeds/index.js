f32da9756ace147b0c92753550bacd6e42ca5bc4
mgetz34 <mgetz340@gmail.com>
2022-12-12 20:04:49 -0700

    added a highscore and users seed files and data

--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,20 @@
 const sequelize = require("../config/connection");
+const seedUser = require('./userData');
+const seedHighScore = require('./highScoreData');
 const seedWords = require('./wordData');
 const seedHints = require('./hintData');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
 
+    await seedUser();
+
+    await seedHighScore();
+
     await seedWords();
 
     await seedHints();
+
 
     process.exit(0);
 };
