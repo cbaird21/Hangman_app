591c871bd231532292bed88f0391cf896213a107
Garrett Anderson <gman.anderson2001@gmail.com>
2022-12-13 13:10:38 -0700

    Added game routes for game page

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
 const highscoreRoutes = require("./highscore-routes");
+const gameRoutes = require("./game-routes");
 
 router.use("/users", userRoutes);
 router.use("/highscores", highscoreRoutes);
+router.use("/game", gameRoutes);
 
 module.exports = router;
